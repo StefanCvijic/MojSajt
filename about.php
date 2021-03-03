d60ba0cffb625f9cff8821394c506df86093835c
Stefan Cvijic <stefan.pet.cvijic@gmail.com>
2021-03-03 18:07:25 +0100

    php changes

--- about.php
+++ about.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Page Title</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Coda&family=Tomorrow&display=swap" rel="stylesheet">  
-        <link rel="stylesheet" href="styles.css">
-    </head>
+    <?php include('header.php'); ?> 
 <body>
     <div class="container">
         <div class="nav-wrapper">
---
+++ header.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Page Title</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Coda&family=Tomorrow&display=swap" rel="stylesheet">  
+    <link rel="stylesheet" href="styles.css">
+</head>
